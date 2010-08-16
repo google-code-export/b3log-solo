426b9111c8ebe3e7cd379efe943f1d39bdb33d78
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2010-08-16 07:57:58 +0000

    Added CSDN blog post.

---
+++ trunk/b3log-solo/src/test/java/org/b3log/solo/csdn/blog/WriterTestCase.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2009, 2010, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.solo.csdn.blog;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import org.b3log.solo.model.Article;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+/**
+ * {@link Writer} test case.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Aug 16, 2010
+ */
+public final class WriterTestCase {
+
+    /**
+     * Tests
+     * {@linkplain Writer#newPost(java.lang.String, java.lang.String, org.b3log.solo.csdn.blog.CSDNBlogArticle) }
+     * method.
+     * @throws Exception exception
+     */
+    @Test
+    public void newPost() throws Exception {
+        final JSONObject article = getArticle();
+        final CSDNBlogArticle csdnBlogArticle = new CSDNBlogArticle(article);
+
+        final CSDNBlog writer = new CSDNBlog();
+        writer.newPost("DL88250", "8825088250", csdnBlogArticle);
+    }
+
+    /**
+     * Gets an article for testing.
+     * 
+     * @return article
+     * @throws JSONException json exception
+     */
+    private JSONObject getArticle() throws JSONException {
+        final JSONObject ret = new JSONObject();
+
+        ret.put(Article.ARTICLE_TITLE,
+                UUID.randomUUID().toString());
+        ret.put(Article.ARTICLE_CREATE_DATE, new Date());
+        ret.put(Article.ARTICLE_CONTENT, "test content");
+        final Set<String> categories = new HashSet<String>();
+        categories.add("475410");
+        ret.put(Article.ARTICLE_TAGS_REF, (Object) categories);
+
+        return ret;
+    }
+}
