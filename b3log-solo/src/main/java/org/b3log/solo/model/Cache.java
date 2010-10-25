452b641e0cd25086db59e895645a18ee770c4a8e
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2010-10-25 03:32:18 +0000

    Init cache report.

---
+++ b3log-solo/src/main/java/org/b3log/solo/model/Cache.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2009, 2010, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.solo.model;
+
+/**
+ * This class defines all cache model relevant keys.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Oct 25, 2010
+ */
+// TODO: move this class into Latke
+public final class Cache {
+
+    /**
+     * Cache.
+     */
+    public static final String CACHE = "cache";
+    /**
+     * Cached count.
+     */
+    public static final String CACHE_CACHED_COUNT = "cacheCachedCount";
+    /**
+     * Cache hit count.
+     */
+    public static final String CACHE_HIT_COUNT = "cacheHitCount";
+    /**
+     * Cache max count.
+     */
+    public static final String CACHE_MAX_COUNT = "cacheMaxCount";
+    /**
+     * Cache miss count.
+     */
+    public static final String CACHE_MISS_COUNT = "cacheMissCount";
+
+    /**
+     * Private default constructor.
+     */
+    private Cache() {
+    }
+}
