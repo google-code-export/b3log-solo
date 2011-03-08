1c7f4a36e63898bff02db32400b642b704f74a4c
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2011-03-08 07:29:15 +0000

    Updates version to 0.2.6.

--- trunk/b3log-solo/src/main/java/org/b3log/solo/SoloServletListener.java
+++ trunk/b3log-solo/src/main/java/org/b3log/solo/SoloServletListener.java
@@ -93,7 +93,7 @@ public final class SoloServletListener extends AbstractServletListener {
     /**
      * B3log Solo version.
      */
-    public static final String VERSION = "0.2.5";
+    public static final String VERSION = "0.2.6";
     /**
      * Logger.
      */
