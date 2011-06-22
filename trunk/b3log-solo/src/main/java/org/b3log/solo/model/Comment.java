d554e0ef9df1dc00ead1762d40cc9cff95f6aa64
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2011-06-22 10:27:03 +0000

    会退到 r2836。在 trunk 上为 issue 206 做得修改被撤回。

--- trunk/b3log-solo/src/main/java/org/b3log/solo/model/Comment.java
+++ trunk/b3log-solo/src/main/java/org/b3log/solo/model/Comment.java
@@ -21,69 +21,67 @@ import java.text.SimpleDateFormat;
 
 /**
  * This class defines all comment model relevant keys.
- * 
+ *
  * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
  * @version 1.0.0.4, Sep 11, 2010
  */
 public final class Comment {
 
-	/**
-	 * Comment.
-	 */
-	public static final String COMMENT = "comment";
-	/**
-	 * Comments.
-	 */
-	public static final String COMMENTS = "comments";
-	/**
-	 * Key of comment.
-	 */
-	public static final String COMMENT_CONTENT = "commentContent";
-	/**
-	 * Key of comment name.
-	 */
-	public static final String COMMENT_NAME = "commentName";
-	/**
-	 * Key of comment email.
-	 */
-	public static final String COMMENT_EMAIL = "commentEmail";
-	/**
-	 * Key of comment URL.
-	 */
-	public static final String COMMENT_URL = "commentURL";
-	/**
-	 * Key of comment sharp URL.
-	 */
-	public static final String COMMENT_SHARP_URL = "commentSharpURL";
-	/**
-	 * Key of comment date.
-	 */
-	public static final String COMMENT_DATE = "commentDate";
-	/**
-	 * Key of comment thumbnail URL.
-	 */
-	public static final String COMMENT_THUMBNAIL_URL = "commentThumbnailURL";
-	/**
-	 * Key of original comment id.
-	 */
-	public static final String COMMENT_ORIGINAL_COMMENT_ID = "commentOriginalCommentId";
-	/**
-	 * Key of original comment user name.
-	 */
-	public static final String COMMENT_ORIGINAL_COMMENT_NAME = "commentOriginalCommentName";
-	/**
-	 * Date format(yyyy/MM/dd hh:mm:ss).
-	 */
-	public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    /**
+     * Comment.
+     */
+    public static final String COMMENT = "comment";
+    /**
+     * Comments.
+     */
+    public static final String COMMENTS = "comments";
+    /**
+     * Key of comment.
+     */
+    public static final String COMMENT_CONTENT = "commentContent";
+    /**
+     * Key of comment name.
+     */
+    public static final String COMMENT_NAME = "commentName";
+    /**
+     * Key of comment email.
+     */
+    public static final String COMMENT_EMAIL = "commentEmail";
+    /**
+     * Key of comment URL.
+     */
+    public static final String COMMENT_URL = "commentURL";
+    /**
+     * Key of comment sharp URL.
+     */
+    public static final String COMMENT_SHARP_URL = "commentSharpURL";
+    /**
+     * Key of comment date.
+     */
+    public static final String COMMENT_DATE = "commentDate";
+    /**
+     * Key of comment thumbnail URL.
+     */
+    public static final String COMMENT_THUMBNAIL_URL = "commentThumbnailURL";
+    /**
+     * Key of original comment id.
+     */
+    public static final String COMMENT_ORIGINAL_COMMENT_ID =
+            "commentOriginalCommentId";
+    /**
+     * Key of original comment user name.
+     */
+    public static final String COMMENT_ORIGINAL_COMMENT_NAME =
+            "commentOriginalCommentName";
+    /**
+     * Date format(yyyy/MM/dd hh:mm:ss).
+     */
+    public static final DateFormat DATE_FORMAT =
+            new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
-	//--------------------- extra Key - no date store
-	
-	public static final String COMMENT_ARTICLE_TITLE = "commentArticleTitle" ;
-	
-
-	/**
-	 * Private default constructor.
-	 */
-	private Comment() {
-	}
+    /**
+     * Private default constructor.
+     */
+    private Comment() {
+    }
 }
