fd4764133a2bb2d225a8ebe1d1a762edb55b35ec
lly219@gmail.com <lly219@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2011-07-10 12:02:53 +0000

    coding...

--- b3log-solo/src/main/webapp/js/admin/preference.js
+++ b3log-solo/src/main/webapp/js/admin/preference.js
@@ -179,8 +179,10 @@ admin.preference = {
                         }
                         
                         // update article signs
-                        for (var i = 1; i < signs.length; i++) {
-                            $("#articleSign" + signs[i].oId).tip("option", "content", signs[i].signHTML === "" ? "该签名档为空" : signs[i].signHTML);
+                        if ( $("#articleSign" + signs[0].oId).length > 1) {
+                            for (var i = 1; i < signs.length; i++) {
+                                $("#articleSign" + signs[i].oId).tip("option", "content", signs[i].signHTML === "" ? "该签名档为空" : signs[i].signHTML);
+                            }
                         }
                         break;
                     case "UPDATE_PREFERENCE_FAIL_":
@@ -194,9 +196,11 @@ admin.preference = {
                         break;
                     default:
                         break;
-                }
+                } 
                 $("#loadMsg").text("");
-            } catch (e) {}
+            } catch (e) {
+                console.error(e);
+            }
         }, requestJSONObject);
     },
     
