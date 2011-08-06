7fbd321d97fe24b1ea649e2679a986ae5ad170bb
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2011-08-06 07:12:13 +0000

    Fixes issue 236.

---
+++ trunk/b3log-solo/src/main/java/org/b3log/solo/fix/TagArticleCounterRepair.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2009, 2010, 2011, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.solo.fix;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.b3log.latke.Keys;
+import org.b3log.latke.action.util.PageCaches;
+import org.b3log.latke.repository.Query;
+import org.b3log.latke.repository.Transaction;
+import org.b3log.latke.util.CollectionUtils;
+import org.b3log.solo.model.Article;
+import org.b3log.solo.model.Tag;
+import org.b3log.solo.repository.ArticleRepository;
+import org.b3log.solo.repository.TagArticleRepository;
+import org.b3log.solo.repository.TagRepository;
+import org.b3log.solo.repository.impl.ArticleGAERepository;
+import org.b3log.solo.repository.impl.TagArticleGAERepository;
+import org.b3log.solo.repository.impl.TagGAERepository;
+import org.b3log.solo.util.Preferences;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Repairs tag article counter.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Aug 5, 2011
+ */
+public final class TagArticleCounterRepair extends HttpServlet {
+
+    /**
+     * Default serial version uid.
+     */
+    private static final long serialVersionUID = 1L;
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER =
+            Logger.getLogger(TagArticleCounterRepair.class.getName());
+    /**
+     * Preference utilities.
+     */
+    private Preferences preferenceUtils = Preferences.getInstance();
+    /**
+     * Tag repository.
+     */
+    private TagRepository tagRepository = TagGAERepository.getInstance();
+    /**
+     * Tag-Article repository.
+     */
+    private TagArticleRepository tagArticleRepository =
+            TagArticleGAERepository.getInstance();
+    /**
+     * Article repository.
+     */
+    private ArticleRepository articleRepository =
+            ArticleGAERepository.getInstance();
+
+    @Override
+    protected void doGet(final HttpServletRequest request,
+                         final HttpServletResponse response)
+            throws ServletException, IOException {
+        final PrintWriter writer = response.getWriter();
+
+        final Transaction transaction = tagRepository.beginTransaction();
+        try {
+            final JSONObject result = tagRepository.get(new Query());
+            final JSONArray tagArray = result.getJSONArray(Keys.RESULTS);
+            final List<JSONObject> tags =
+                    CollectionUtils.jsonArrayToList(tagArray);
+            for (final JSONObject tag : tags) {
+                final String tagId = tag.getString(Keys.OBJECT_ID);
+                final JSONObject tagArticleResult =
+                        tagArticleRepository.getByTagId(tagId, 1,
+                                                        Integer.MAX_VALUE);
+                final JSONArray tagArticles =
+                        tagArticleResult.getJSONArray(Keys.RESULTS);
+                final int tagRefCnt = tagArticles.length();
+                int publishedTagRefCnt = 0;
+                for (int i = 0; i < tagRefCnt; i++) {
+                    final JSONObject tagArticle = tagArticles.getJSONObject(i);
+                    final String articleId = tagArticle.getString(
+                            Article.ARTICLE + "_" + Keys.OBJECT_ID);
+                    final JSONObject article = articleRepository.get(articleId);
+                    final boolean isPublished =
+                            article.getBoolean(Article.ARTICLE_IS_PUBLISHED);
+                    if (isPublished) {
+                        publishedTagRefCnt++;
+                    }
+                }
+
+                tag.put(Tag.TAG_REFERENCE_COUNT, tagRefCnt);
+                tag.put(Tag.TAG_PUBLISHED_REFERENCE_COUNT, publishedTagRefCnt);
+
+                tagRepository.update(tagId, tag);
+            }
+
+            transaction.commit();
+            PageCaches.removeAll();
+            
+            writer.println("Repair sucessfully!");
+        } catch (final Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            writer.println("Repairs failed, error msg[" + e.getMessage() + "]");
+        } finally {
+            writer.close();
+        }
+    }
+}
