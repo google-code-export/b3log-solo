e579b007af5b97ed0bc25a4fd9f8043420a780f3
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2011-08-16 05:07:18 +0000

    修改发送评论邮件：完善日志打印以备排错。

--- b3log-solo/src/main/java/org/b3log/solo/action/impl/AddArticleCommentAction.java
+++ b3log-solo/src/main/java/org/b3log/solo/action/impl/AddArticleCommentAction.java
@@ -50,7 +50,6 @@ import org.b3log.solo.event.EventTypes;
 import org.b3log.solo.jsonrpc.impl.CommentService;
 import org.b3log.solo.model.Article;
 import org.b3log.solo.model.Comment;
-import org.b3log.solo.model.Google;
 import org.b3log.solo.model.Page;
 import org.b3log.solo.model.Preference;
 import org.b3log.solo.repository.ArticleCommentRepository;
@@ -70,7 +69,7 @@ import org.json.JSONObject;
  * Adds article comment action.
  *
  * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
- * @version 1.0.0.5, Aug 8, 2011
+ * @version 1.0.0.6, Aug 16, 2011
  */
 public final class AddArticleCommentAction extends AbstractAction {
 
@@ -384,7 +383,7 @@ public final class AddArticleCommentAction extends AbstractAction {
                 comment.getString(Comment.COMMENT_SHARP_URL);
         final Message message = new Message();
         message.setFrom(adminEmail);
-        message.addRecipient(preference.getString(Preference.ADMIN_EMAIL));
+        message.addRecipient(adminEmail);
         String mailSubject = null;
         String articleOrPageURL = null;
         String mailBody = null;
@@ -422,8 +421,8 @@ public final class AddArticleCommentAction extends AbstractAction {
                 replace("{commenter}", commenter);
         message.setHtmlBody(mailBody);
         LOGGER.log(Level.FINER,
-                   "Sending a mail[mailSubject={0}, mailBody=[{1}] to admins",
-                   new Object[]{mailSubject, mailBody});
+                   "Sending a mail[mailSubject={0}, mailBody=[{1}] to admin[email={2}]",
+                   new Object[]{mailSubject, mailBody, adminEmail});
         mailService.send(message);
     }
 
@@ -444,20 +443,6 @@ public final class AddArticleCommentAction extends AbstractAction {
     }
 
     /**
-     * Gets comment sharp URL with the specified page and comment id.
-     *
-     * @param page the specified page
-     * @param commentId the specified comment id
-     * @return comment sharp URL
-     * @throws JSONException json exception
-     */
-    private String getCommentSharpURLForPage(final JSONObject page,
-                                             final String commentId)
-            throws JSONException {
-        return page.getString(Page.PAGE_PERMALINK) + "#" + commentId;
-    }
-
-    /**
      * Sets commenter thumbnail URL for the specified comment.
      *
      * @param comment the specified comment
@@ -469,42 +454,6 @@ public final class AddArticleCommentAction extends AbstractAction {
         final String id = commentEmail.split("@")[0];
         final String domain = commentEmail.split("@")[1];
         String thumbnailURL = null;
-
-        // Try to set thumbnail URL using Google Buzz API
-        if ("gmail.com".equals(domain.toLowerCase())) {
-            final URL googleProfileURL =
-                    new URL(Google.GOOGLE_PROFILE_RETRIEVAL.replace("{userId}",
-                                                                    id));
-            try {
-                final HTTPRequest request = new HTTPRequest();
-                request.setURL(googleProfileURL);
-                final HTTPResponse response = urlFetchService.fetch(request);
-                final int statusCode = response.getResponseCode();
-
-                if (HttpServletResponse.SC_OK == statusCode) {
-                    final byte[] content = response.getContent();
-                    final String profileJSONString =
-                            new String(content, "UTF-8");
-                    LOGGER.log(Level.FINEST, "Google profile[jsonString={0}]",
-                               profileJSONString);
-                    final JSONObject profile = new JSONObject(profileJSONString);
-                    final JSONObject profileData = profile.getJSONObject("data");
-                    thumbnailURL = profileData.getString("thumbnailUrl");
-                    comment.put(Comment.COMMENT_THUMBNAIL_URL, thumbnailURL);
-                    LOGGER.log(Level.FINEST, "Comment thumbnail[URL={0}]",
-                               thumbnailURL);
-
-                    return;
-                } else {
-                    LOGGER.log(Level.WARNING,
-                               "Can not fetch google profile[userId={0}, statusCode={1}]",
-                               new Object[]{id, statusCode});
-                }
-            } catch (final Exception e) {
-                LOGGER.log(Level.WARNING,
-                           "Can not fetch google profile[userId=" + id + "", e);
-            }
-        }
 
         // Try to set thumbnail URL using Gravatar service
         final String hashedEmail = MD5.hash(commentEmail.toLowerCase());
