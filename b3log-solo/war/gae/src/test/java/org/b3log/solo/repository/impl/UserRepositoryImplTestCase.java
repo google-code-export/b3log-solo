fa5a39a0e98b89714c4468e589c34cecd1c36970
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2011-12-29 05:20:08 +0000

    加入 repository 单元测试。

---
+++ b3log-solo/war/gae/src/test/java/org/b3log/solo/repository/impl/UserRepositoryImplTestCase.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2009, 2010, 2011, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.solo.repository.impl;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalMemcacheServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.b3log.latke.Keys;
+import org.b3log.latke.Latkes;
+import org.b3log.latke.model.Role;
+import org.b3log.latke.model.User;
+import org.b3log.latke.repository.FilterOperator;
+import org.b3log.latke.repository.Query;
+import org.b3log.latke.repository.Transaction;
+import org.b3log.solo.repository.UserRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * {@link UserRepositoryImpl} test case.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Dec 29, 2011
+ */
+public final class UserRepositoryImplTestCase {
+
+    /**
+     * Local service test helper.
+     */
+    private final LocalServiceTestHelper localServiceTestHelper =
+            new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig(),
+                                       new LocalMemcacheServiceTestConfig());
+    /**
+     * User repository.
+     */
+    private UserRepository userRepository;
+
+    /**
+     * Before class.
+     */
+    @BeforeClass
+    public void beforeClass() {
+        localServiceTestHelper.setUp();
+
+        Latkes.initRuntimeEnv();
+
+        userRepository = UserRepositoryImpl.getInstance();
+    }
+
+    /**
+     * After class.
+     */
+    @AfterClass
+    public void afterClass() {
+        localServiceTestHelper.tearDown();
+
+        Latkes.shutdown();
+    }
+
+    /**
+     * Tests add user.
+     * 
+     * @throws Exception exception
+     */
+    @Test
+    public void test() throws Exception {
+        final JSONObject user = new JSONObject();
+        user.put(User.USER_NAME, "test");
+        user.put(User.USER_EMAIL, "test@gmail.com");
+        user.put(User.USER_PASSWORD, "pass");
+        user.put(User.USER_ROLE, Role.ADMIN_ROLE);
+
+        final JSONObject another = new JSONObject();
+        another.put(User.USER_NAME, "test1");
+        another.put(User.USER_EMAIL, "test1@gmail.com");
+        another.put(User.USER_PASSWORD, "pass1");
+        another.put(User.USER_ROLE, Role.DEFAULT_ROLE);
+
+        final Transaction transaction = userRepository.beginTransaction();
+        
+        userRepository.add(user);
+        userRepository.add(another);
+        
+        transaction.commit();
+
+        Assert.assertTrue(userRepository.isAdminEmail("test@gmail.com"));
+
+
+        final JSONObject admin = userRepository.getAdmin();
+
+        Assert.assertNotNull(admin);
+        Assert.assertEquals("test", admin.optString(User.USER_NAME));
+
+        final JSONObject result =
+                userRepository.get(new Query().addFilter(User.USER_NAME,
+                                                         FilterOperator.EQUAL,
+                                                         "test1"));
+
+        final JSONArray users = result.getJSONArray(Keys.RESULTS);
+        Assert.assertEquals(users.length(), 1);
+        Assert.assertEquals(users.getJSONObject(0).getString(User.USER_EMAIL),
+                            "test1@gmail.com");
+    }
+}
