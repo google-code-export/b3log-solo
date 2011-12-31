7c26a5ea604b123217d051585c07ad94cf363837
DL88250@gmail.com <DL88250@gmail.com@f3ee095b-c205-7ee6-81c1-d2bd65ac1524>
2011-12-31 04:55:33 +0000

    持久化单元测试

---
+++ trunk/b3log-solo/core/src/test/java/org/b3log/solo/repository/impl/FileRepositoryImplTestCase.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2009, 2010, 2011, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.solo.repository.impl;
+
+import org.b3log.solo.AbstractTestCase;
+import org.b3log.solo.repository.FileRepository;
+import org.testng.annotations.Test;
+
+/**
+ * {@link FileRepositoryImpl} test case.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Dec 31, 2011
+ */
+public class FileRepositoryImplTestCase extends AbstractTestCase {
+
+    /**
+     * Tests.
+     * 
+     * @throws Exception exception
+     */
+    @Test
+    public void test() throws Exception {
+        final FileRepository fileRepository = getFileRepository();
+    }
+}
